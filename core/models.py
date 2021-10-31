c5dba54b7b0a880e159ed48a1ce963d5a927d644
Pedro Antonio <p.antonio.f.s@gmail.com>
2021-10-31 14:17:59 -0300

    Create Model Asset

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .facade import ModelFacade
 
 # Create your models here.
+class Asset(models.Model):
+    investor = ModelFacade.create_ForeignKey(User)
+    name = ModelFacade.create_CharField("Ativo", 10)
+    price = ModelFacade.create_MoneyField("Preço", 5)
+    max_limit = ModelFacade.create_MoneyField("Limite máximo", 5)
+    min_limit = ModelFacade.create_MoneyField("Limite mínimo", 5)
+    sleep_time = ModelFacade.create_DurationField("Periodicidade de atualização")
+
+    class Meta:
+        verbose_name = "Ativo"
+        unique_together = ['investor', 'name']
