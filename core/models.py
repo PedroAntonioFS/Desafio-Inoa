3bfe138927a5c4237147f9f07515fc52777dae58
Pedro Antonio <p.antonio.f.s@gmail.com>
2021-11-06 18:26:34 -0300

    Unused import deleted

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .facade import ModelFacade, B3Facade
+from .facade import ModelFacade
 
 class Asset(models.Model):
     investor = ModelFacade.create_ForeignKey(User)
