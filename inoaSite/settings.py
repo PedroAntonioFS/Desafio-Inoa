16d40a8f7cb0324c2f44018eac1eca0cf83a96e5
Pedro Antonio <p.antonio.f.s@gmail.com>
2021-11-07 16:38:46 -0300

    Create BaseAssetNotifier class #25

--- inoaSite/settings.py
+++ inoaSite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+json_file = open("email_configs.json")
+email_data = json.load(json_file)
+
+EMAIL_BACKEND = email_data['EMAIL_BACKEND']
+EMAIL_HOST = email_data['EMAIL_HOST']
+EMAIL_USE_TLS = "True" == email_data['EMAIL_USE_TLS']
+EMAIL_PORT = int(email_data['EMAIL_PORT'])
+EMAIL_HOST_USER = email_data['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = email_data['EMAIL_HOST_PASSWORD']
