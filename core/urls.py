a4d9d8978a405fe57a333f74fd7ddd1359666328
Pedro Antonio <p.antonio.f.s@gmail.com>
2021-11-13 16:37:20 -0300

    Create view CreateUserView #44

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/', AddAssetView.as_view(), name="add_asset"),
     path('<pk>/update/', UpdateAssetView.as_view(), name="update_asset"),
     path('<pk>/delete/', DeleteAssetView.as_view(), name="delete_asset"),
+    path('sign_up/', CreateUserView.as_view(), name="sign_up")
 ]
